b931448d5b875b172396b209c900150e5cc995f3
sergiohdljr <sergio.tjf1@gmail.com>
2026-05-27 11:43:33 -0300

    Update axios dependency and implement file upload functionality in UsersPage component
    Modified package.json and package-lock.json to update the axios dependency to version 1.16.1. Enhanced the UsersPage component by adding file upload capabilities, including state management for file selection and an asynchronous submit function to handle file uploads to the server.

--- apps/web/src/modules/users/presentation/pages/UsersPage.tsx
+++ apps/web/src/modules/users/presentation/pages/UsersPage.tsx
@@ -1,10 +1,43 @@
-import { CreateUserForm } from '../components/CreateUserForm';
+import axios from "axios";
+import { useState } from "react";
 
 export function UsersPage() {
+
+  const [file, setFile] = useState<File | null>()
+
+  const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFile(e.target.files?.[0] ?? null);
+  }
+
+  const submit = async () => {
+    if (!file) {
+      return
+    }
+
+    const { size, type, name } = file
+
+
+    const { data: uploadId } = await axios.post("http://localhost:3000/api/v1/files/upload",
+      {
+        filename: name,
+        mimeType: type,
+        size: size,
+        folderId: null,
+        ownerId: "a865afb2-9769-4c29-be08-75eb2ccda5ab"
+      }
+    )
+
+    console.log(uploadId)
+  }
+
   return (
     <main>
-      <h1>Users</h1>
-      <CreateUserForm />
+
+      <input onChange={onChangeFile} type="file" />
+      <br></br>
+      <br></br>
+      <button onClick={submit} >fazer</button>
+
     </main>
   );
 }
